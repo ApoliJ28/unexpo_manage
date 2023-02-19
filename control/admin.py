e0ef3c24b1ebc8047dd9ce2a531ef36866710d64
Raniel666 <74976226+Raniel666@users.noreply.github.com>
2023-02-19 14:49:07 -0400

    Modificacion del la Clase USER
    Agregado carrera como Opciones 
    Asignando superusuario
    Username =  admin1
    password = 12345@

--- control/admin.py
+++ control/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Asignatura, User
 # Register your models here.
+# Permite añadir los modelos al panale de administradorr
+admin.site.register(Asignatura)
+admin.site.register(User)
+# SuperUsuario
+# User: admin1
+# password: 1234@
--- control/models.py
+++ control/models.py
@@ -1,3 +1,39 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
+
+
+class Asignatura(models.Model):
+    Codigo = models.IntegerField(primary_key=True, unique=True)
+    Nombre = models.CharField(max_length=100)
+    Especialidad = models.CharField(max_length=100)
+    Unidades = models.IntegerField()
+    CantidadMax = models.IntegerField()
+    Abierta = models.BooleanField(default=False)
+    Unidades = models.IntegerField()
+
+    # Para que lo podemos visualizar en el administrardoe
+
+    def __str__(self):
+        return self.Nombre + " su cargo es  " + self.Especialidad
+
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    id = models.AutoField(primary_key=True, verbose_name=u"ID")
+    opciones_carrera = [('In', 'Ingieneria Industrial'), ('Mec', 'Ingieneria Mecanica'),
+                        ('Sis', 'Ingieneria Sistemas')]
+    carrera = models.CharField(
+        choices=opciones_carrera, max_length=15, verbose_name=u'Carrera')
+    # opciones_modalidad = [('P', 'Parcial'), ('C', 'Completo')]
+    # TipoEstudiante = models.CharField(
+    #     choices=opciones_modalidad, max_length=15, verbose_name=u'Estudiante')
+
+    def __str__(self):
+        return self.user.username + " " + self.carrera
