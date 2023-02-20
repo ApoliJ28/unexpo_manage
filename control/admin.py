fc92821022838a3665e8c9518454742517703a2d
Raniel666 <74976226+Raniel666@users.noreply.github.com>
2023-02-20 14:28:18 -0400

    Delete control directory

--- control/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Asignatura, User
-# Register your models here.
-# Permite añadir los modelos al panale de administradorr
-admin.site.register(Asignatura)
-admin.site.register(User)
-# SuperUsuario
-# User: admin1
-# password: 1234@
