d191d39c6aa407fa178bcb1b42eb749249d1645d
ApoliJ28 <apolinaresvictor@gmail.com>
2023-02-21 04:03:40 +0100

    Se agregaron las opciones editar y eliminar al formulario de asignaturas Una vez la asignatura ya ingreso con un codigo este sera su llave primaria y no se podra cambiar

--- Academico/urls.py
+++ Academico/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+
 # Acceder a directorio raiz
 urlpatterns = [
     path('pensum/', views.asignatura_admin, name= "pensum"),
     path('', views.inicio, name = "inicio" ),
     path('salir/', views.salir, name = "salir"),
+    path('pensum/eliminacionCurso/<codigo>', views.eliminarCurso),
+    path('pensum/edicionCurso/<codigo>', views.edicionCurso, name= "editarCurso"),
+    path('editarCurso/', views.editarCurso, name = "editar"),
 ]
--- Academico/views.py
+++ Academico/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from .forms import AsignaturaForm
 from .models import Asignatura
+from django.contrib import messages
 
 @login_required
 
@@ -20,11 +21,60 @@ def asignatura_admin(request):
             if form.is_valid():
                 form.save()
                 # Datos correctos
+                messages.success(request, "Curso Registrado!")
                 return redirect('/pensum')
         except ValueError:
             return render(request, '/pensum', {"asignaturas": asignaturas, "form": form, 'error': 'Ingresa valores correctamente'})
 
+    messages.success(request, "Cursos listados!")
+
     return render(request, "materia.html", {"asignaturas": asignaturas, "form": form})
+
+@login_required
+
+def eliminarCurso(request, codigo):
+    asignatura = Asignatura.objects.get(codigo=codigo)
+    asignatura.delete()
+
+    messages.success(request, "Curso Eliminado!")
+
+    return redirect("/pensum")
+
+@login_required
+
+def edicionCurso(request, codigo):
+    asignatura = Asignatura.objects.get(codigo=codigo)
+    return render(request, "edicion_materia.html", {"asignatura":asignatura})
+
+@login_required
+
+def editarCurso(request):
+    codigo = request.POST['codigo']
+    nombre = request.POST['nombre']
+    unidades = request.POST['num_unidades']
+    credito_requerido = request.POST['credito_requerido']
+    cantidadmax_estudiantes = request.POST['cantidadmax_estudiantes']
+    cantidad_estudiantes = request.POST['cantidad_estudiantes']
+    carrera =  request.POST['carrera']
+    
+    abierta = request.POST.get('abierta',False)
+    abierta_bool = False
+    if abierta == 'on':
+        abierta_bool = True
+
+    asignatura = Asignatura.objects.get(codigo=codigo)
+    asignatura.nombre = nombre
+    asignatura.unidades = unidades
+    asignatura.credito_requerido = credito_requerido
+    asignatura.cantidadmax_estudiantes = cantidadmax_estudiantes
+    asignatura.cantidad_estudiantes = cantidad_estudiantes
+    asignatura.abierta = abierta_bool
+    asignatura.carrera = carrera
+    asignatura.save()
+
+    messages.success(request, "Curso Actualizado!")
+
+    return redirect("/pensum")
 
 @login_required
 
