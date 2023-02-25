d614e1b5f177aa51219f2899d3b832a4f443d1bc
Victor Apolinares <apolinaresvictor@gmail.com>
2023-02-25 17:59:44 +0100

    Se termina de subir todos los archivos del carrito en este commit

---
+++ Academico/context_process.py
@@ -0,0 +1,6 @@
+def total(request):
+    total_creditos = 0
+    if request.user.is_authenticated and request.session.get("carrito"):
+        for key, value in request.session["carrito"].items():
+            total_creditos += int(value["unidades"])
+    return {"total_creditos" : total_creditos}
