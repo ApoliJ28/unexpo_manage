c2d2a15c006a391302450102b5e331900efdfcb4
Moises <msira270997@gmail.com>
2023-04-07 18:46:21 -0400

    El total de creditos ya no se saca del carrito sino directo del registro de inscripcion. Se agrego la cuenta del total a pagar

--- Academico/context_process.py
+++ Academico/context_process.py
@@ -1,6 +1,15 @@
+from .models import RegistroInscripcion
+
+
 def total(request):
     total_creditos = 0
-    if request.user.is_authenticated and request.session.get("carrito"):
-        for key, value in request.session["carrito"].items():
-            total_creditos += int(value["unidades"])
-    return {"total_creditos": total_creditos}
+    estudiante_id = request.user.id
+    registro_inscripcion = RegistroInscripcion.objects.filter(
+        estudiante_id=estudiante_id
+    )
+    if request.user.is_authenticated and registro_inscripcion:
+        for registro in registro_inscripcion:
+            materias = registro.materias_ids.all()
+            for materia in materias:
+                total_creditos += materia.creditos
+    return {"total_creditos": total_creditos, "pago_total": total_creditos*3}
